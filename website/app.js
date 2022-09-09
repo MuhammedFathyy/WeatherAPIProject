a45031d2a6e40e3a6438370a65b1e34e801a8dd9
MuhammedFathyy <mfathy56734@gmail.com>
2022-09-09 23:21:45 +0200

    enviroment setup

---
+++ website/app.js
@@ -0,0 +1,5 @@
+/* Global Variables */
+const apiKey = '6c7badbc49988c7087b5c5362f24b804&units=imperial';
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
