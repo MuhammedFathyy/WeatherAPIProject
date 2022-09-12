1b7de1bbed21a70351eb52791a4688b92c67e121
MuhammedFathyy <mfathy56734@gmail.com>
2022-09-12 18:41:45 +0200

    second update

--- website/app.js
+++ website/app.js
@@ -1,5 +1,25 @@
 /* Global Variables */
-const apiKey = '6c7badbc49988c7087b5c5362f24b804&units=imperial';
+
+const { query } = require("express");
+
+const apiKey = '6c7badbc49988c7087b5c5362f24b804&units=metric';
 // Create a new date instance dynamically with JS
+
+const generateInfo = document.querySelector('#zip');
+
+generateInfo.addEventListener('clock' , async () =>
+{
+
+    const zipCode = document.querySelector('zip').value;
+    
+    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}`;
+
+    const res = await fetch(url);
+
+    
+
+})
+
+
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
